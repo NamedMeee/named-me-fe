92987a5b8f506ce7257e2c92c3cd96ab5cfc5e40
ire4564 <ire4564@gmail.com>
2023-11-15 20:50:30 +0900

    feat: svg import 설정

--- src/components/common/MenuBar.tsx
+++ src/components/common/MenuBar.tsx
@@ -1,7 +1,9 @@
+import Logo from '@assets/svgs/Logo.svg';
+
 export default function MenuBar() {
   return (
-    <div className="flex w-full h-[56px] p-4 justify-center items-center flex-shrink-0 bg-black shadow-md">
-      상단 바
+    <div className="flex w-full h-[56px] p-4 justify-center items-center flex-shrink-0 bg-white shadow-md">
+      <Logo />
     </div>
   );
 }
