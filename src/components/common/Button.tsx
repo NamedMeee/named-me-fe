25a7de3e686e8a963145492aec48b74161d9fba9
ire4564 <ire4564@gmail.com>
2024-03-12 20:41:58 +0900

    feat: 로그인 화면 컴포넌트 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,6 +2,7 @@ interface ButtonProps {
   buttonStyles?: string;
   buttonColor?: string;
   textColor?: string;
+  border?: boolean;
   text: string | React.ReactNode;
 }
 
@@ -10,15 +11,18 @@ export default function Button({
   buttonColor,
   textColor,
   text,
+  border = false,
 }: ButtonProps) {
   return (
     <div className="flex justify-center">
       <button
-        className={`${buttonStyles} items-center w-full ${
+        className={`${
+          border && 'border'
+        } ${buttonStyles} font_label_medium_lg items-center w-full ${
           buttonColor ? buttonColor : 'surface_primary_inverse_active'
         } ${
           textColor ? textColor : 'content_primary_inverse'
-        } rounded-[6px] py-[16px]`}
+        } rounded-[8px] py-[16px]`}
       >
         {text}
       </button>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,9 @@
 import MobileLayout from '@components/layout/MobileLayout';
 
 import SmallLogo from '@assets/svgs/SmallLogo.svg';
+import KakaoLogo from '@assets/svgs/KakaoLogo.svg';
+import TwitterLogo from '@assets/svgs/TwitterLogo.svg';
+
 import { Button } from '@components/common';
 
 export default function Main() {
@@ -28,16 +31,53 @@ export default function Main() {
             <div className="font_label_bold_lg mb-[6px]">비밀번호</div>
             <input
               className="border border-[#D5D5D5] font_label_regular_lg rounded-[6px] px-[18px] py-[18px] w-full"
-              type="text"
+              type="password"
               placeholder="비밀번호를 입력해주세요."
             />
           </div>
         </div>
       </div>
-      <Button text="로그인" buttonStyles={'mt-[48px] w-[320px]'} />
-      <div className="gap-y-[12px]">
-        <Button text="로그인" buttonStyles={'w-[320px]'} />
-        <Button text="로그인" buttonStyles={'w-[320px]'} />
+      <div className="flex justify-center">
+        <div className="w-[320px]">
+          <div className="relative border-b-[1px] border_disabled mb-[24px] pb-[24px]">
+            <Button
+              text="로그인"
+              buttonStyles={'mb-[20px] mt-[48px] w-[320px]'}
+            />
+            <div className="flex justify-center">
+              <div className="content_tertiary w-full absolute p-4 bg-white w-[118px] font_label_regular_sm text-center">
+                계정이 없다면 🥹
+              </div>
+            </div>
+          </div>
+          <Button
+            buttonColor="bg-[#FEEA2A]"
+            text={
+              <div className="flex justify-center items-center gap-x-[6px]">
+                <KakaoLogo className="w-[16px] h-[16px]" />
+                카카오로 시작하기
+              </div>
+            }
+            buttonStyles="mb-[12px]"
+            textColor="text-[#333334]"
+          />
+          <Button
+            buttonColor="bg-[#5A9BE7]"
+            text={
+              <div className="flex justify-center items-center gap-x-[2px]">
+                <TwitterLogo className="w-[24px] h-[24px]" />
+                트위터로 시작하기
+              </div>
+            }
+            buttonStyles="mb-[12px]"
+          />
+          <Button
+            border={true}
+            buttonColor="bg-white"
+            textColor="content_primary"
+            text={<div>📧 이메일로 시작하기</div>}
+          />
+        </div>
       </div>
     </MobileLayout>
   );
